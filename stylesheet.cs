ce47f97f62a5187f2b20725e380c6ed6f6080b53
sarayparra <106513957+sarayparra@users.noreply.github.com>
2022-08-16 21:07:15 -0600

    Create stylesheet.cs

---
+++ stylesheet.cs
@@ -0,0 +1,35 @@
+/*style*/
+/* CSS CODE */
+/*HEADING */
+h1{
+    color: red;
+    font-size: 45px;
+}
+/* 3 by 3 grid */
+#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9{
+    width: 80px;
+    height: 52px;
+    margin: auto;
+    border: 1px solid gray;
+    border-radius: 6px;
+    font-size: 30px;
+    text-align: center;
+}
+/* reset button */
+#but{
+    box-sizing: border-box;
+    width: 95px;
+    height: 55px;
+    border: 1 px solid blueviolet;
+    margin: auto;
+    border-radius: 6px;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    background-color: blueviolet;
+    color: whitesmoke;
+    font-size: 25px;
+    cursor: pointer;
+}
+/*main container */
+#main{
+    text-align: center;
+}
